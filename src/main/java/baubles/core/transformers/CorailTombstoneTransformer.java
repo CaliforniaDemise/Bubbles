8fb9f682b1c1a06e42d0f5124d95c43ae16060a6
CaliforniaDemise <vwwwwv@proton.me>
2025-03-23 18:07:10 +0300

    Properly sort items Corail Tombstone. - Chooses the right bauble instead of a random item in players inventory while putting the baubles.

--- src/main/java/baubles/core/transformers/CorailTombstoneTransformer.java
+++ src/main/java/baubles/core/transformers/CorailTombstoneTransformer.java
@@ -3,19 +3,14 @@ package baubles.core.transformers;
 import baubles.api.BaublesApi;
 import baubles.api.IBauble;
 import baubles.api.cap.IBaublesItemHandler;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntIterator;
-import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.ints.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.ItemStackHandler;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.tree.ClassNode;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class CorailTombstoneTransformer extends BaseTransformer {
 
@@ -37,37 +32,29 @@ public class CorailTombstoneTransformer extends BaseTransformer {
     @SuppressWarnings("unused")
     public static void $autoEquip(EntityPlayer player, ItemStackHandler corailTombstone, List<Integer> ids, boolean isRespawn) {
         IBaublesItemHandler handler = BaublesApi.getBaublesHandler(player);
-        IntList slots = new IntArrayList(8);
-        List<ItemStack> list = new ArrayList<>(8);
+        Int2ObjectMap<ItemStack> map = new Int2ObjectOpenHashMap<>();
         {
-            Iterator<Integer> iterator = ids.iterator();
+            Iterator<Integer> iterator = ids.stream().sorted(Comparator.reverseOrder()).iterator();
             while (iterator.hasNext()) {
                 int i = iterator.next();
                 ItemStack stack = corailTombstone.getStackInSlot(i);
                 if (!stack.isEmpty()) {
                     IBauble bauble = BaublesApi.getBauble(stack);
                     if (bauble != null) {
-                        list.add(stack);
-                        slots.add(i);
-                        iterator.remove();
+                        map.put(i, stack);
                     }
                 }
             }
         }
-        Iterator<ItemStack> iterator = list.iterator();
-        IntIterator intIterator = slots.iterator();
-        while (iterator.hasNext()) {
-            int a = intIterator.nextInt();
-            ItemStack stack = iterator.next();
-            IBauble bauble = Objects.requireNonNull(BaublesApi.getBauble(stack));
+        for (Int2ObjectMap.Entry<ItemStack> entry : map.int2ObjectEntrySet()) {
+            IBauble bauble = Objects.requireNonNull(BaublesApi.getBauble(entry.getValue()));
             for (int i = 0; i < handler.getSlots(); i++) {
                 ItemStack baubleStack = handler.getStackInSlot(i);
                 if (!baubleStack.isEmpty()) continue;
-                if (bauble.canEquip(stack, player) && bauble.canPutOnSlot(handler, i, stack)) {
-                    handler.setStackInSlot(i, stack);
-                    bauble.onEquipped(stack, player);
-                    corailTombstone.setStackInSlot(a, ItemStack.EMPTY);
-                    iterator.remove();
+                if (bauble.canEquip(entry.getValue(), player) && bauble.canPutOnSlot(handler, i, entry.getValue())) {
+                    handler.setStackInSlot(i, entry.getValue());
+                    bauble.onEquipped(entry.getValue(), player);
+                    corailTombstone.setStackInSlot(entry.getIntKey(), ItemStack.EMPTY);
                     break;
                 }
             }
