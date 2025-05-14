82c281ef9befaede2297644bdb067a97ea37ad02
CaliforniaDemise <vwwwwv@proton.me>
2025-05-14 11:46:59 +0300

    Schedule creative cleanup task.

--- src/main/java/baubles/core/transformers/CreativeInvTransformer.java
+++ src/main/java/baubles/core/transformers/CreativeInvTransformer.java
@@ -5,6 +5,8 @@ import baubles.api.cap.IBaublesItemHandler;
 import baubles.common.network.PacketHandler;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -46,10 +48,13 @@ public class CreativeInvTransformer extends BaseTransformer {
         @Override public void toBytes(ByteBuf buf) {}
         @Override
         public IMessage onMessage(PacketCreativeClean message, MessageContext ctx) {
-            IBaublesItemHandler handler = BaublesApi.getBaublesHandler(ctx.getServerHandler().player);
-            for (int i = 0; i < handler.getSlots(); i++) {
-                handler.setStackInSlot(i, ItemStack.EMPTY);
-            }
+            IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+            mainThread.addScheduledTask(() -> {
+                IBaublesItemHandler handler = BaublesApi.getBaublesHandler(ctx.getServerHandler().player);
+                for (int i = 0; i < handler.getSlots(); i++) {
+                    handler.setStackInSlot(i, ItemStack.EMPTY);
+                }
+            });
             return null;
         }
     }
